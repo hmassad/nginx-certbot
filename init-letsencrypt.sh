e8796c54b9c6050ce09a8f1c251a5d83a3f70e70
Hernán Massad <hernan.massad@gmail.com>
2020-03-12 16:28:24 -0300

    added email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(smart-factory.app www.smart-factory.app)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="aws@smart-factory.app" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
